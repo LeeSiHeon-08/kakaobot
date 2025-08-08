3bbed8831efd91b2c85640b7eda081e50367e2ee
LeeSiHeon-08 <lsiheon@naver.com>
2025-08-09 00:13:21 +0900

    Update ch04_kakaobot.py

--- ch04_kakaobot.py
+++ ch04_kakaobot.py
@@ -73,7 +73,7 @@ def getTextFromGPT(messages):
     ]
     try:
         response = client.chat.completions.create(
-            model="gpt-4o-mini",
+            model="gpt-5",
             messages=messages_prompt
         )
         return response.choices[0].message.content
